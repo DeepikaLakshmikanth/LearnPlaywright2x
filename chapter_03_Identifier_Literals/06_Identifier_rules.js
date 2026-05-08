b5a9cd0d16b1345ad3755c369ae343f998d0f5d9
DeepikaLakshmikanth <deepikas.aryan@gmail.com>
2026-05-08 09:51:45 +0530

    Add chapter 2 and chapter 3 content

---
+++ chapter_03_Identifier_Literals/06_Identifier_rules.js
@@ -0,0 +1,21 @@
+var $ = 10;
+var _ = 20;
+var p = 40;
+var a1 = 30;
+
+var Name = "Pramod";
+var name = "Amit";
+
+var pramod_dutta = "hello";
+var pramod$dutta = "hello";
+var pramodu1232 = "hello";
+
+/** These are the rules for valid identifiers in JavaScript: 
+
+• Must begin with a letter (a–z, A–Z), underscore (_), or dollar sign ($).
+• Subsequent characters may also include digits (0–9).
+• Cannot start with a digit.
+• Cannot be a reserved keyword.
+• Are case-sensitive.
+• May contain Unicode letters and Unicode escape sequences.
+• Cannot contain spaces, hyphens, or special characters (except _ and $). **/
---
+++ chapter_03_Identifier_Literals/08_Comments.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+// Single Line Comments starts with // in the beginning where as the multi line comments start with  /* to indicate the the beginning 
+//of the comment block where as */ are used to indicate end of the comment block 
